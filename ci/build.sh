8edf13803c7c91dcc0bd40d54cccb31763f572b5
Nicholas Bruce <saltair93+github@gmail.com>
2019-05-31 11:59:04 -0700

    apparently can't delete the build folder

--- ci/build.sh
+++ ci/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Nuke existing website
-rm -rf ../build_output
-mkdir ../build_output
+# rm -rf ../build_output
+# mkdir ../build_output
 
 # Build lab_manual
 bash ./lab_manual/gen_html.sh
