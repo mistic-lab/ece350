340f25d9495249020970f61d1c19208a15a08833
Nicholas Bruce <saltair93+github@gmail.com>
2019-05-31 14:09:54 -0700

    tweaky

--- ci/build.sh
+++ ci/build.sh
@@ -8,7 +8,7 @@ script_dir="$(dirname $0)"
 html_dir="${script_dir}/../build_output"
 
 echo "#1: "
-ls ${html_dir}/
+ls ${html_dir}
 
 # Build lab_manual
 bash ${script_dir}/lab_manual/gen_html.sh
@@ -16,7 +16,7 @@ bash ${script_dir}/lab_manual/gen_html.sh
 # Shove in index.html and grc_doc
 cp ${script_dir}/index.html ${html_dir}
 echo "#6: "
-ls ${html_dir}/
+ls ${html_dir}
 # mkdir ${html_dir}/grc_doc
 # cp -r ${script_dir}/grc_doc/* ${html_dir}/grc_doc
 
