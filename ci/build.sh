600541fdef8ff313fb719a0203895772689d2544
Nicholas Bruce <saltair93+github@gmail.com>
2019-05-31 15:02:37 -0700

    forever trying new things

--- ci/build.sh
+++ ci/build.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-# Nuke existing website
-# rm -rf ../build_output
-# mkdir ../build_output
-
 script_dir="$(dirname $0)"
 html_dir="${script_dir}/../build_output"
 
-echo "#1: "
-ls ${html_dir}
 
 # Build lab_manual
 bash ${script_dir}/lab_manual/gen_html.sh
 
 # Shove in index.html and grc_doc
 cp ${script_dir}/index.html ${html_dir}
-echo "#6: "
-ls ${html_dir}
+
+#TODO Shove in grc_doc
 # mkdir ${html_dir}/grc_doc
 # cp -r ${script_dir}/grc_doc/* ${html_dir}/grc_doc
 
-#? Celebrate
-# echo "Woohoo?"
--- ci/lab_manual/gen_html.sh
+++ ci/lab_manual/gen_html.sh
@@ -16,26 +16,17 @@ if [ $? != 0 ] # I xmllint threw an error, bail out with exit code 1
     exit 1
 fi
 
-#TODO Doesn't exist anymore
-# # Move what is currently published and make space for ant
-# mkdir ${html_dir}/lab_manual_old
-# mv ${html_dir}/lab_manual/* ${html_dir}/lab_manual_old 
-# rm -rf ${html_dir}/lab_manual
-echo "#3: "
-ls ${html_dir}
 
 # Render the HTML
 ant webhelp ${script_dir}/build.xml
-echo "#4: "
-ls ${html_dir}
 
 # Copy to directory
 if [ $? == 0 ] # If ant did not throw an error
   then 
     cp -r ${script_dir}/../../lab_manual/data ${html_dir}/lab_manual
-    # rm -rf ${html_dir}/lab_manual_old
+elif [ $? != 0]
+  then
+  echo "ANT COMPLAINED"
 fi
-echo "#5: "
-ls ${html_dir}
 
-# chmod -R o+r ${html_dir}/lab_manual
+
