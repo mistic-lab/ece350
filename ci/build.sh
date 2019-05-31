25f2d7c5df03383d5d16b8b64e96f76878130c8c
Nicholas Bruce <saltair93+github@gmail.com>
2019-05-31 13:36:20 -0700

    Is this a directory structure? *butterfly*

--- ci/build.sh
+++ ci/build.sh
@@ -5,15 +5,15 @@
 # mkdir ../build_output
 
 script_dir="$(dirname $0)"
-html_dir=${script_dir}/../build_output
+html_dir="${script_dir}/../build_output"
 
 # Build lab_manual
 bash ${script_dir}/lab_manual/gen_html.sh
 
 # Shove in index.html and grc_doc
 cp ${script_dir}/index.html ${html_dir}
-mkdir ${html_dir}/grc_doc
-cp -r ${script_dir}/grc_doc/* ${html_dir}/grc_doc
+# mkdir ${html_dir}/grc_doc
+cp -r ${script_dir}/grc_doc ${html_dir}
 
 #? Celebrate
 echo "Woohoo?"
