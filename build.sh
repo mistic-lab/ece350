782fc692176c4661b96e0b0b6d9519502ef7c290
Nicholas Bruce <saltair93+github@gmail.com>
2019-06-03 09:22:48 -0700

    ALL IN

--- build.sh
+++ build.sh
@@ -5,20 +5,21 @@
 
 
 ########### Build lab_manual
-cd lab_manual
+# cd lab_manual
 ## Lint the XML
 xmllint --valid lab_manual/lab_manual.xml --noout
 ## Render the HTML
 ant webhelp
 
 # TODO Copy to directory
-# cp -r data ../build_output/lab_manual
+cp -r lab_manual/data build_output/lab_manual
+cd ..
 
 ########## Shove in grc_doc
 #TODO Shove in grc_doc
-# mkdir ${html_dir}/grc_doc
-# cp -r ${script_dir}/grc_doc/* ${html_dir}/grc_doc
+mkdir build_output/grc_doc
+cp -r grc_doc/build_output/* build_output/grc_doc
 
 ########## Add static site elements
 # TODO Shove in index.html and grc_doc
-# cp index.html ${html_dir}
+cp static_site/index.html build_output
