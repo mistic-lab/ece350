772bb59e388252b0c6e5c91333f45f6dd33df12b
Nicholas Bruce <saltair93+github@gmail.com>
2019-06-03 10:02:45 -0700

    Commenting stuff

--- build.sh
+++ build.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-
-# script_dir="$(dirname $0)"
-# html_dir="${script_dir}/../build_output"
-
+#########################
+#
+# Builds the ECE350 course website 
+# Build is done by Travis CI and published to Github Pages
+# 
+# Date: June 2019
+# Author: Nicholas Bruce
+#
+#########################
 
 ########### Build lab_manual
 cd lab_manual
-## Lint the XML
+# Lint the XML
 xmllint --valid lab_manual.xml --noout
-## Render the HTML
+# Render the HTML
 ant webhelp
-
-# TODO Copy to directory
+# Copy data for lab_manual
 cp -r data ../build_output/lab_manual
 cd ..
+
 ########## Shove in grc_doc
-#TODO Shove in grc_doc
 mkdir build_output/grc_doc
 cp -r grc_doc/build_output/* build_output/grc_doc
 
 ########## Add static site elements
-# TODO Shove in index.html and grc_doc
 cp static_site/index.html build_output
