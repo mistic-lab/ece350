d5675b250b76c1b01729dc0ffa9fb4d2ab76dded
Nicholas Bruce <saltair93+github@gmail.com>
2019-06-03 09:26:21 -0700

    had to be something wrong in there right

--- build.sh
+++ build.sh
@@ -5,20 +5,20 @@
 
 
 ########### Build lab_manual
-# cd lab_manual
+cd lab_manual
 ## Lint the XML
-xmllint --valid lab_manual/lab_manual.xml --noout
+xmllint --valid lab_manual.xml --noout
 ## Render the HTML
 ant webhelp
 
 # TODO Copy to directory
-cp -r lab_manual/data build_output/lab_manual
-cd ..
+# cp -r lab_manual/data build_output/lab_manual
+# cd ..
 
 ########## Shove in grc_doc
 #TODO Shove in grc_doc
-mkdir build_output/grc_doc
-cp -r grc_doc/build_output/* build_output/grc_doc
+# mkdir build_output/grc_doc
+# cp -r grc_doc/build_output/* build_output/grc_doc
 
 ########## Add static site elements
 # TODO Shove in index.html and grc_doc
